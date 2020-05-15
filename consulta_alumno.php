463bafb4c566bfed7d12186907f973a85d36a81c
Angel León <angelleon.ef@gmail.com>
2020-05-15 00:45:14 -0400

    arreglar fitro

--- consulta_alumno.php
+++ consulta_alumno.php
@@ -1,33 +1,22 @@
 <?php
   
- $conexion=mysqli_connect('localhost','root','','servidor.cobro');
+    $conexion=mysqli_connect('localhost','root','','servidor.cobro');
+    $v1=$_POST['numer'];
+    $sql="  SELECT codigo,nombre,observacion,celular,grado,seccion 
+            FROM alumnos 
+            WHERE codigo='$v1'";
+    $stmt1 = mysqli_query($conexion, $sql);
  
+    $row=mysqli_fetch_array($stmt1, MYSQLI_NUM);
 
- $v1=$_POST['numer'];
+    $aux0=$row['0'];
+    $aux1=$row['1'];
+    $aux2=$row['2'];
+    $aux3=$row['3']; 
+    $aux4=$row['4']; $aux5=$row['5']; 
 
- var_dump($v1);
-
- $sql="SELECT codigo,nombre,observacion,celular,grado,seccion FROM alumnos WHERE codigo='$v1'";
- $stmt1 = mysqli_query($conexion, $sql);
- 
- $row=mysqli_fetch_array($stmt1, MYSQLI_NUM);
-
-
-
-             $aux0=$row['0'];
-             $aux1=$row['1'];
-             $aux2=$row['2'];
-             $aux3=$row['3']; 
-             $aux4=$row['4']; 
-             $aux5=$row['5']; 
-            
-
-              if ($aux0) 
-              {
-               $res='1&'.$aux1.'&'.$aux2.'&'.$aux3.'&'.$aux4.'&'.$aux5.'&';
-               echo  $res;
-              
-              }
-              
-
+    if ($aux0) {
+        $res='1&'.$aux1.'&'.$aux2.'&'.$aux3.'&'.$aux4.'&'.$aux5.'&';
+        echo  $res;
+    }
 ?>
